659e09eb72be3bbfe752eab18eaab152c5646557
kunpengjiang <argentea@pku.edu.cn>
2021-12-07 15:59:46 +0800

    change defalt action to main

--- myutils.py
+++ myutils.py
@@ -3,6 +3,7 @@ from utils.diffData import diffData
 from utils.process import poolData
 import numpy as np
 
-data = np.loadtxt("/home/kunpengjiang/project/gpuEikonal/build/gpufsmlog", float)
-data = np.reshape(data, -1, 'C')
-drawjet(data, "nothing")
+if __name__ == "__main__":
+    data = np.loadtxt("/home/kunpengjiang/project/gpuEikonal/build/gpufsmlog", float)
+    data = np.reshape(data, -1, 'C')
+    drawjet(data, "nothing")
