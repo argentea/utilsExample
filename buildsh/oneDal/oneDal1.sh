ff03536ee5f687c14cb0f32403d35e7a9e8d4c14
kunpeng <kunpeng.jiang@intel.com>
2022-08-10 13:19:54 +0800

    OneDal compile sh

---
+++ buildsh/oneDal/oneDal1.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+__conda_setup="$('$HOME/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+conda activate oneDal
+#conda list
+
+#source /opt/intel/oneapi/tbb/latest/env/vars.sh intel64
+source /opt/intel/oneapi/compiler/latest/env/vars.sh
+export CPATH=$HOME/project/oneDAL/__deps/mklgpufpk/lnx/include:$CPATH
+export CPATH=$HOME/anaconda3/envs/oneDal/include/linux:$HOME/anaconda3/envs/oneDal/include:$CPATH
+
+
+make onedal -j7 PLAT=lnx32e COMPILER=gnu
