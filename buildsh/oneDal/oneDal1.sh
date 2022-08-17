55db2e5fc18ca6f400d96248b6d6d2581e152b37
kunpeng <kunpeng.jiang@intel.com>
2022-08-17 16:33:21 +0000

    Rewrite oneDal build script
    Add auto download
    Delete unnecessary requirement
    	only add oneapi dpcpp
    Clarify dependency

--- buildsh/oneDal/oneDal1.sh
+++ buildsh/oneDal/oneDal1.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
-_oneDalHome=$HOME/test/oneDAL
-_oneDalHome:=$HOME/project/oneDAL
+#Please set your oneDal source home and intel oneapi toolkit home properly
+
+_oneDalHome=$HOME/project/oneDAL
+_intelOneapiHome=$HOME/intel/oneapi
+
+#Suppose you're using anaconda and use oneDal env for oneDal building
 
 __conda_setup="$('$HOME/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
@@ -18,13 +22,34 @@ unset __conda_setup
 conda activate oneDal
 #conda list
 
+#discarded env variable config
 #source /opt/intel/oneapi/tbb/latest/env/vars.sh intel64
-source /opt/intel/oneapi/compiler/latest/env/vars.sh
-export CPATH=$HOME/project/oneDAL/__deps/mklgpufpk/lnx/include:$CPATH
+#source /opt/intel/oneapi/compiler/latest/env/vars.sh
+
+
+#Find dpcpp, which is necessary for target onapi
+source $_intelOneapiHome/compiler/latest/env/vars.sh
+if [ ! command -v dpcpp &> /dev/null ]; then
+	echo "dpcpp is not installed properly, please check the install path"
+	exit
+fi
+
+#Setup mklgpufpk
+_mklgpuPATH=$_oneDalHome/__deps/mklgpufpk
+export CPATH=$_mklgpuPATH/lnx/include:$CPATH
+
+##Find or install mklgpufpk
+if [ -d $_mklgpuPATH ]; then
+	echo "mklgpu found!"
+else
+	$_oneDalHome/dev/download_micromkl.sh
+fi
+
+#Setup openjdk path
+#Which is necessary for daal
 export CPATH=$HOME/anaconda3/envs/oneDal/include/linux:$HOME/anaconda3/envs/oneDal/include:$CPATH
 
 
 cd $_oneDalHome;
-make oneapi -j7 PLAT=lnx32e COMPILER=gnu
-make daal -j7  PLAT=lnx32e COMPILER=gnu
-
+make oneapi -j$(expr $(nproc) - 2) PLAT=lnx32e COMPILER=gnu &&\
+	make daal -j$(expr $(nproc) - 2)  PLAT=lnx32e COMPILER=gnu
