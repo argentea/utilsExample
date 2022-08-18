5043c3ff520617b05ea47136b50d4e1372f46a68
kunpeng <kunpeng.jiang@intel.com>
2022-08-18 12:52:46 +0000

    Change the order of make
    It seems oneapi have dependency on daal

--- buildsh/oneDal/oneDal1.sh
+++ buildsh/oneDal/oneDal1.sh
@@ -45,11 +45,23 @@ else
 	$_oneDalHome/dev/download_micromkl.sh
 fi
 
+#Setup tbb
+_tbbPATH=$_oneDalHome/__deps/tbb
+#export CPATH=$_tbbPATH/lnx/include:$CPATH
+
+##Find or install tbbfpk
+if [ -d $_tbbPATH ]; then
+	echo "tbb found!"
+else
+	$_oneDalHome/dev/download_tbb.sh
+fi
+
 #Setup openjdk path
 #Which is necessary for daal
 export CPATH=$HOME/anaconda3/envs/oneDal/include/linux:$HOME/anaconda3/envs/oneDal/include:$CPATH
 
-
+#build from source
+#Attention!! You have to build daal before build oneapi
 cd $_oneDalHome;
-make oneapi -j$(expr $(nproc) - 2) PLAT=lnx32e COMPILER=gnu &&\
-	make daal -j$(expr $(nproc) - 2)  PLAT=lnx32e COMPILER=gnu
+make daal -j$(expr $(nproc) - 2)  PLAT=lnx32e COMPILER=gnu && \
+	make oneapi -j$(expr $(nproc) - 2) PLAT=lnx32e COMPILER=gnu
